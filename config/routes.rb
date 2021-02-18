e959015283052406c60244dc3fae0d09541586e2
RomariY <yaremakroma@gmail.com>
2021-02-18 13:25:34 +0200

    Create and stylized the log in  form

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   get 'static_pages/news'
   #get 'home/index'
   root 'static_pages#home'
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
