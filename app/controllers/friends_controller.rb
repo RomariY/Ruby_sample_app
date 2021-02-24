4bae89e64ac9973b281a8b12c393d012f6adbf7d
RomariY <yaremakroma@gmail.com>
2021-02-24 12:42:14 +0200

    Make home page when sign out and friend page when log i like a root

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -13,7 +13,8 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+    #@friend = Friend.new
+    @friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -22,7 +23,8 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
+    #@friend = Friend.new(friend_params)
+    @friend = current_user.friends.build
 
     respond_to do |format|
       if @friend.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root 'friends#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'static_pages#home', as: :unauthenticated_root
+    end
+  end
   resources :friends
-  #get 'static_pages/home'
+  get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/news'
   #get 'home/index'
-  root 'static_pages#home'
+  root 'friends#index'
+  #root 'static_pages#home'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
